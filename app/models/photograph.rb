91f5738c056f9ea939ae73c42877989413a66fb6
Joe <joezjwang@gmail.com>
2015-05-19 07:45:37 +0000

    changed compression parameters

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -3,6 +3,6 @@ class Photograph < ActiveRecord::Base
 	has_many :collections, :through => :collectionphotographs
 	has_one :category
 
-	has_attached_file :photo, :styles => {:thumb => "400x400>#", :large => "1200x900", :small => "800x600"}, :convert_options => { :thumb => '-quality 60 -strip', :small => '-quality 60 -strip'},  :default_url => "/missing.png"
+	has_attached_file :photo, :styles => {:original => "1600x1600>", :thumb => "400x400>#", :large => "1200x900", :small => "800x600"}, :convert_options => { :thumb => '-quality 85 -strip', :small => '-quality 85 -strip -interlace Plane', :large => '-quality 85 -strip -interlace Plane'},  :default_url => "/missing.png"
 	validates_attachment_content_type :photo, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png)$/
 end
