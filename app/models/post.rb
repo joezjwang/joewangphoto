ab444f49ee2a2dfb56dd73db68c519b2120e3ec0
Joe <joezjwang@gmail.com>
2015-11-12 12:10:06 +0000

    mocked up post listing view and generated post body preview in model

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,13 @@ class Post < ActiveRecord::Base
 	def should_generate_new_friendly_id?
 		slug.blank? || title_changed?
 	end
+
+	def body_preview_pc
+		#should count the number of words and reduce the sentence count if it is too long
+		body.split(/\.\s+/, 8)[0...7].join(' ').concat('...')
+	end
+	def body_preview_mobile
+		#should count the number of words and reduce the sentence count if it is too long
+		body.split(/\.\s+/, 4)[0...3].join(' ').concat('...')
+	end
 end
