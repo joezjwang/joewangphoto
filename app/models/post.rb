ccdab7559f89fa6cb3e84b4aac2ef473eaff2632
Joe <joezjwang@gmail.com>
2015-11-12 15:34:10 +0000

    seperated mobile and pc views of post listing

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,16 @@ class Post < ActiveRecord::Base
 		slug.blank? || title_changed?
 	end
 
+	def reading_time
+		#count words.convert to float and divide by 275 plus number of photos*0.5 and round the end result
+	end
+	def photo_count
+		#count number of photos associated with post. Count in database relation or in body?
+	end
+
 	def body_preview_pc
 		#should count the number of words and reduce the sentence count if it is too long
-		body.split(/\.\s+/, 8)[0...7].join(' ').concat('...')
+		body.split(/\.\s+/, 7)[0...6].join(' ').concat('...')
 	end
 	def body_preview_mobile
 		#should count the number of words and reduce the sentence count if it is too long
