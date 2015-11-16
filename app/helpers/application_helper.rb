2f8d55d7bd5c00ec0f6b663ffe129cbe62bb7d38
Joe <joezjwang@gmail.com>
2015-11-16 22:49:36 +0000

    added open graph meta info to collection show views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,19 @@
 module ApplicationHelper
 
-
     def title(text)
     content_for :title, text
   	end
 
   	def description(text)
     content_for :description, text
+  	end
+
+  	def og_image_url(text)
+    content_for :og_image_url, text
+  	end
+
+  	def og_page_url(text)
+    content_for :og_page_url, text
   	end
   	
 end
