3c37df5b39fa05ba5983a4add0f74ea7f2ef91a3
Joe <joezjwang@gmail.com>
2015-11-17 23:03:31 +0000

    Added markdown to views and introduced wordcount and reading time to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,19 +8,31 @@ class Post < ActiveRecord::Base
 		slug.blank? || title_changed?
 	end
 
+	def word_count
+		body.scan(/[[:alpha:]]+/).count
+	end
+
 	def reading_time
 		#count words.convert to float and divide by 275 plus number of photos*0.5 and round the end result
+		(word_count.to_f/275).round
 	end
 	def photo_count
 		#count number of photos associated with post. Count in database relation or in body?
 	end
 
+	#Ideally this function should be based on number of words and cut off at the end of sentences
+	def body_preview (number_of_sentences)
+		body.split(/\.\s+/, number_of_sentences+1)[0...number_of_sentences].join(' ').concat('...')
+	end
+	def body_preview_admin
+		body_preview(4)
+	end
 	def body_preview_pc
 		#should count the number of words and reduce the sentence count if it is too long
-		body.split(/\.\s+/, 7)[0...6].join(' ').concat('...')
+		body_preview(4)
 	end
 	def body_preview_mobile
 		#should count the number of words and reduce the sentence count if it is too long
-		body.split(/\.\s+/, 4)[0...3].join(' ').concat('...')
+		body_preview(2)
 	end
 end
