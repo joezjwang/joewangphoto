96228d10c1ea6bccdab4155c35b1f18338be9462
Joe <joezjwang@gmail.com>
2015-12-10 04:50:06 +0000

    Redcarpet image renderer is overwritten to work with paperclip and html5 figure tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,9 +24,47 @@ module ApplicationHelper
       disable_indented_code_blocks: true
     }
 
-    renderer = Redcarpet::Render::HTML.new(options)
+    renderer = PaperclipRender.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
 
     markdown.render(text).html_safe
+  end
+end
+
+class PaperclipRender < Redcarpet::Render::HTML
+  include ActionView::Helpers::AssetTagHelper
+    
+  # my image-object here  has_attached_file: :data
+  # we overwrite Redcarpet::Render::HTML#image
+  #
+  def image(link_or_id, style_and_css_or_alt, title)  
+    if link_or_id.to_i > 0
+      args = style_and_css_or_alt.split("|")
+      style = args[0].to_sym
+      css = args[1] 
+      # .find_by here b'cause it won't raise ActiveRecord::RecordNotFound
+      # if none found - no image output
+      if img = Blogimage.find_by(id: link_or_id)
+        #image_tag(img.image.url(style), title: title, alt: img.title, class: css)
+      
+        "<figure><img src='#{img.image.url(style)}' alt='#{img.title}'></figure><figurecap>#{img.caption}</figurecap>"
+      else
+        ''
+      end
+    else
+      # just do it like Redcarpet::Render::HTML would do
+      # so we can still use default image markdown
+      image_tag(link_or_id, title: title, alt: style_and_css_or_alt)
+    end
+  end
+  def preprocess(full_document)
+    full_document.gsub(/\[figure:(.*)\]/) do
+      image = Blogimage.find_by(title: $1)
+      if image
+        "<span>#{$1}</span>"
+      else
+        $1
+      end 
+    end
   end
 end
