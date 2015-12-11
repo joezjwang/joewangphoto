599617d4fb9c0f2f54f7f50e8d566eaef4808a5e
Joe <joezjwang@gmail.com>
2015-12-11 02:45:07 +0000

    Code highlighting now works with Markdown through pygments and redcarpet

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 #for markdown
 gem 'redcarpet'
+#for syntax hightlighing in markdown
+gem 'pygments.rb', '~> 0.6.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,30 +10,30 @@ module ApplicationHelper
   	end
   	
   	def markdown(text)
+
+    renderer = CustomMardownRenders.new(hard_wrap: true, filter_html: true)
     options = {
-      filter_html:     true,
-      hard_wrap:       true, 
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true, 
-      fenced_code_blocks: true
-    }
-
-    extensions = {
-      autolink:           true,
+      autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      lax_html_blocks: true,
       superscript:        true,
-      disable_indented_code_blocks: true
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      strikethrough: true
     }
 
-    renderer = PaperclipRender.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
-
-    markdown.render(text).html_safe
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
 end
 
-class PaperclipRender < Redcarpet::Render::HTML
+class CustomMardownRenders < Redcarpet::Render::HTML
   include ActionView::Helpers::AssetTagHelper
-    
+
+  def block_code(code, language)
+    Pygments.highlight(code, lexer:language)
+  end
   # my image-object here  has_attached_file: :data
   # we overwrite Redcarpet::Render::HTML#image
   #
@@ -46,8 +46,7 @@ class PaperclipRender < Redcarpet::Render::HTML
       # if none found - no image output
       if img = Blogimage.find_by(id: link_or_id)
         #image_tag(img.image.url(style), title: title, alt: img.title, class: css)
-      
-        "<figure><img src='#{img.image.url(style)}' alt='#{img.title}'></figure><figurecap>#{img.caption}</figurecap>"
+        "<figure><img src='#{img.image.url(style)}' alt='#{img.caption}'><br/><figcaption>#{img.caption}</figcaption></figure>"
       else
         ''
       end
@@ -57,14 +56,14 @@ class PaperclipRender < Redcarpet::Render::HTML
       image_tag(link_or_id, title: title, alt: style_and_css_or_alt)
     end
   end
-  def preprocess(full_document)
-    full_document.gsub(/\[figure:(.*)\]/) do
-      image = Blogimage.find_by(title: $1)
-      if image
-        "<span>#{$1}</span>"
-      else
-        $1
-      end 
-    end
-  end
+  # def preprocess(full_document)
+  #   full_document.gsub(/\[figure:(.*)\]/) do
+  #     image = Blogimage.find_by(title: $1)
+  #     if image
+  #       "<span>#{$1}</span>"
+  #     else
+  #       $1
+  #     end 
+  #   end
+  # end
 end
