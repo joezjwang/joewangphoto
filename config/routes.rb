3e94c9612e60b5fc00feee2674b1d005fd1db35e
Joe <joezjwang@gmail.com>
2015-12-24 06:10:55 +0000

    menu styles changes, pointed home to collection index thus skipping the slide show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'photographs#index'
+  root 'collections#index'
 
   namespace :admin do
     root to: 'admin#index'
