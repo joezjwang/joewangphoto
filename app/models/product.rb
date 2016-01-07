c42aeec92f0e895da19342a92cf755046d8d307e
Joe <joezjwang@gmail.com>
2016-01-07 00:50:54 +0000

    added mobile views to shop

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	validates :shopify_product_handle, :shopify_product_id, :name, presence: :true
-	has_attached_file :product_photo, :styles => {:original => "1600x1600>",  :pc => "x400", :mobile => "300x"}, :convert_options => { :pc => '-quality 80 -interlace Plane', :mobile => '-quality 80 -interlace Plane'},  :default_url => "/missing.png"
+	has_attached_file :product_photo, :styles => {:original => "1600x1600>",  :pc => "x400", :mobile => "450x"}, :convert_options => { :pc => '-quality 80 -interlace Plane', :mobile => '-quality 75 -interlace Plane'},  :default_url => "/missing.png"
 	validates_attachment_content_type :product_photo, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png)$/
 	validates_attachment_size :product_photo, :less_than => 10.megabytes
 end
