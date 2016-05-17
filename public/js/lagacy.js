ad4518deddef0ce180019c63aff8dca50668c68c
Joe <joezjwang@gmail.com>
2016-05-17 05:30:35 +0000

    revert to older version to solve menu issue

--- public/js/lagacy.js
+++ public/js/lagacy.js
@@ -1,4 +1,6 @@
-  $(window).scroll(function() {
+$(function() {
+
+    $(window).scroll(function() {
         var height = $(window).scrollTop();
         if (height>400)
         {
@@ -12,6 +14,7 @@
         }
 
     });
+  });
 // $( document ).ready(function() {
 //     $(".collection-photo").mouseover(function(){
 //     console.log("mouseover");
@@ -35,12 +38,12 @@ $(document).keydown(function(e) {
 
         if ($('#lightbox').css("display")=="none"){
             $('#lightbox').fadeIn();
-            //$('#menu-section ul').fadeOut();
-            $('#nav-section .logo').fadeOut();
+            $('#menu-section ul').fadeOut();
+            $('#nav-section .logo,.copyright').fadeOut();
             $('.keyboard-instructions').text("press 'L' to turn ON lights");
 
         }
-        else if ($('#nav-section').css("display")=="block")
+        else if ($('#nav-section .copyright').css("display")=="none")
         {
             //console.log($('#lightbox').css("display"));
             $('#menu-section ul').fadeIn();
@@ -49,7 +52,7 @@ $(document).keydown(function(e) {
                 $('#nav-section .logo').fadeIn();
 
             $('.keyboard-instructions').text("press 'L' to turn OFF lights");
-            $('#nav-section .keyboard-instructions').fadeIn();
+            $('#nav-section .keyboard-instructions,.copyright').fadeIn();
             $('#lightbox').fadeOut();
         }
         break;
@@ -73,123 +76,31 @@ $(document).keydown(function(e) {
 });
 
 
+function initPage() {
+
 
 $(".toggle").click(function(){
     $("#menu-section ul").slideToggle();
 });
 
 
-function changePhotoHeight () {
-    pageHeight = jQuery(window).height();
-    pageWidth = jQuery(window).width();
+    var pageHeight = jQuery(window).height();
+    var pageWidth = jQuery(window).width();
 
-    if (pageWidth>899) {
+    if (pageWidth>768) {
         var photoHeight = pageHeight-50;
         var photoWidth = photoHeight*3/2;
-        console.log(photoWidth);
+        //console.log(photoWidth);
         
-        $('.photograph').css({ "max-height": photoHeight + 'px' });
+        $('.collection-photo').css({ "max-height": photoHeight + 'px' });
+        //$('.collection-photo').css({ "width": photoWidth + 'px' });
     }
-  }
-
-  window.onload = changePhotoHeight;
+}
 
 
-
-
-// $(function() {
-
-//     $(window).scroll(function() {
-//         var height = $(window).scrollTop();
-//         if (height>400)
-//         {
-//             //console.log(height);
-//             $("#nav-section .logo").fadeOut();
-//         }
-//         else
-//         {
-//             if ($('#nav-section .copyright').css("display")=="block")
-//                 $("#nav-section .logo").fadeIn();
-//         }
-
-//     });
-//   });
-// // $( document ).ready(function() {
-// //     $(".collection-photo").mouseover(function(){
-// //     console.log("mouseover");
-// //     $(".photo-text-container").fadeIn();
-// //     });
-// //     $(".collection-photo").mouseout(function(){
-// //     $(".photo-text-container").fadeOut();
-// //     });
-// // });
-
-
-//     // function scrollToAnchor(aid){
-//     //     var aTag = $("a[name='"+ aid +"']");
-//     //     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-//     // }
-
-// $(document).keydown(function(e) {
-
-//       switch(e.which) {
-//         case 76: // L key
-
-//         if ($('#lightbox').css("display")=="none"){
-//             $('#lightbox').fadeIn();
-//             //$('#menu-section ul').fadeOut();
-//             $('#nav-section .logo').fadeOut();
-//             $('.keyboard-instructions').text("press 'L' to turn ON lights");
-
-//         }
-//         else if ($('#nav-section').css("display")=="block")
-//         {
-//             //console.log($('#lightbox').css("display"));
-//             $('#menu-section ul').fadeIn();
-
-//             if  ($(window).scrollTop()<400)
-//                 $('#nav-section .logo').fadeIn();
-
-//             $('.keyboard-instructions').text("press 'L' to turn OFF lights");
-//             $('#nav-section .keyboard-instructions').fadeIn();
-//             $('#lightbox').fadeOut();
-//         }
-//         break;
-
-//         case 37: // left
-//         break;
-
-//         case 38: // up
-//         break;
-
-//         case 39: // right
-//         break;
-
-//         case 40: // down
-//         break;
-
-//         default: return; // exit this handler for other keys
-//     }
-//     e.preventDefault(); // prevent the default action (scroll / move caret)
-
-// });
-
-
-
-// $(".toggle").click(function(){
-//     $("#menu-section ul").slideToggle();
-// });
-
-
-//     var pageHeight = jQuery(window).height();
-//     var pageWidth = jQuery(window).width();
-
-//     if (pageWidth>768) {
-//         var photoHeight = pageHeight-50;
-//         var photoWidth = photoHeight*3/2;
-//         //console.log(photoWidth);
-        
-//         $('.collection-photo').css({ "max-height": photoHeight + 'px' });
-//         //$('.collection-photo').css({ "width": photoWidth + 'px' });
-//     }
-
+$(function() {
+  initPage();
+});
+$(window).bind('page:change', function() {
+  initPage();
+});
